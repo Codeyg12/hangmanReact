ff55234091b5d8991645d9c01cb3b9b5152da663
Codeyg12 <codey.gallup@gmail.com>
2022-11-20 19:05:09 -0700

    keyboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
 import { useState } from "react";
-import words from './words.json'
-import './App.css'
-import { HangmanDrawing } from './components/HangmanDrawing';
-import { HangmanWord } from './components/HangmanWord';
-import { Keyboard } from './components/Keyboard'
+import words from "./words.json";
+import "./App.css";
+import { HangmanDrawing } from "./components/HangmanDrawing";
+import { HangmanWord } from "./components/HangmanWord";
+import { Keyboard } from "./components/Keyboard";
 
 function App() {
-  const random = words[Math.floor(Math.random() * words.length)]
-  const [wordToGuess, setWordToGuess] = useState(random)
+  const random = words[Math.floor(Math.random() * words.length)];
+  const [wordToGuess, setWordToGuess] = useState(random);
 
-  const [guessedLettters, setGuessedLetters] = useState([])
+  const [guessedLettters, setGuessedLetters] = useState([]);
 
   return (
     <div className="main">
       <div className="status">Lose Win</div>
       <HangmanDrawing />
       <HangmanWord />
-      <Keyboard />
+      <div style={{ alignSelf: "stretch" }}>
+        <Keyboard />
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
---
+++ src/components/Keyboard.jsx
@@ -0,0 +1,38 @@
+const KEYS = [
+    "a",
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
+]
+
+export function Keyboard() {
+    return (
+        <div class='keyboard'>
+            {KEYS.map(key => {
+                return <button className="btn" key={key}>{key}</button>
+            })}
+        </div>
+    )
+}
